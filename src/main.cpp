fee28154086ff8b9f0b3dd7f011d30076d46345b
Xoaquin Castrelo <xoaquin.cb@gmail.com>
2022-05-12 14:17:30 +0100

    Added support for specifying pins as arguments on the command line.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "serial.hpp"
 #include <QList>
 
+#include <iostream>
+
 #include "window.h"
 #include "ui_window.h"
 
@@ -27,11 +29,31 @@ int main(int argc, char *argv[])
     // needs to run with root via sudo in terminal.
     // wiringPiSetup();
     // pinMode (0, OUTPUT);
+    
+    int pin_scl = 0;
+    int pin_sda = 1;
+    
+    if (argc == 3)
+    {
+        char *arg1 = argv[1];
+        char *arg2 = argv[2];
+        
+        pin_scl = atoi(arg1);
+        pin_sda = atoi(arg2);
+        
+        if (pin_scl == pin_sda)
+        {
+            std::cout << "SCL and SDA pins cannot be the same." << std::endl;
+            exit(1);
+        }
+    }
+    
+    std::cout << "SCL pin: " << pin_scl << ", SDA pin: " << pin_sda << std::endl;
 
     // setup Qt GUI
     QApplication a(argc, argv);
     Window window;
-    Serial serial(0, 1);
+    Serial serial(pin_scl, pin_sda);
     window.show();
 
     QObject::connect(window.ui->centralWidget, &canvas::sendPacket, &serial, &Serial::write);
