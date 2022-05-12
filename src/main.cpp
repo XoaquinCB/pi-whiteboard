10a2944b968a1137b8fcdea9f912bfb44b344022
Xoaquin Castrelo <xoaquin.cb@gmail.com>
2022-05-12 14:19:28 +0100

    Removed unused "worker" thread.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <QDebug>
 #include <QObject>
 
-#include <pthread.h>
 #include <wiringPi.h>
 
 #include "serial.hpp"
@@ -12,16 +11,6 @@
 
 #include "window.h"
 #include "ui_window.h"
-
-void* worker(void* thread_id)
-{
-    long tid = (long)thread_id;
-    // do something....
-    qDebug() << "Worker thread " << tid << "started.";
-
-    // end thread
-    pthread_exit(NULL);
-}
 
 int main(int argc, char *argv[])
 {
@@ -59,22 +48,10 @@ int main(int argc, char *argv[])
     QObject::connect(window.ui->centralWidget, &canvas::sendPacket, &serial, &Serial::write);
     QObject::connect(&serial, &Serial::packet_received, window.ui->centralWidget, &canvas::packetReceived);
 
-    // starting worker thread(s)
-    int rc;
-    pthread_t worker_thread;
-    rc = pthread_create(&worker_thread, NULL, worker, (void*)1);
-    if (rc) {
-        qDebug() << "Unable to start worker thread.";
-        exit(1);
-    }
-
     // start window event loop
     qDebug() << "Starting event loop...";
     int ret = a.exec();
     qDebug() << "Event loop stopped.";
-
-    // cleanup pthreads
-    pthread_exit(NULL);
 
     // exit
     return ret;
